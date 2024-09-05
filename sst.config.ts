10fbe81fa10694219ed8a269aa292298de561fd2
Saul Chavez <romsc93@live.com.mx>
2024-09-05 10:18:08 -0600

    Renamed project in files

--- sst.config.ts
+++ sst.config.ts
@@ -3,7 +3,7 @@
 export default $config({
   app(input) {
     return {
-      name: "monorepo-template",
+      name: "notes2409",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
     };
